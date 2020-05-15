d8b46606f8379df9b1aa0fc55a3d78e366ebacd5
U-AMDX34GB-PC\papa <muh_kosta@mail.ru>
2020-05-15 22:24:42 +0300

    img_matr

--- examples/brainy/Lay.py
+++ examples/brainy/Lay.py
@@ -7,9 +7,9 @@ class Lay:
         self.in_ = None  # количество входов слоя
         self.out = None  # количество выходов слоя
         self.matrix = list([])  # матрица весов
-        self.cost_signals = [0] * max_rows_orOut  # вектор взвешенного состояния нейронов
-        self.hidden = [0] * (max_spec_elems)  # вектор после функции активации
-        self.errors = [0] * (max_spec_elems)  # вектор ошибок слоя
+        self.cost_signals = [0] * max_spec_elems  # вектор взвешенного состояния нейронов
+        self.hidden = [0] * max_spec_elems  # вектор после функции активации
+        self.errors = [0] * max_spec_elems # вектор ошибок слоя
         for row in range(max_rows_orOut):# создаем матрицу весов
             self.inner_m = list([0] * (max_in_nn))  # подготовка матрицы весов,внутренняя матрица
             self.matrix.append(self.inner_m)
