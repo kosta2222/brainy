9a1fd5f728c5dab0f6b7616dfb332596fd947703
U-AMDX34GB-PC\papa <muh_kosta@mail.ru>
2020-05-16 19:19:47 +0300

    img_matr

--- examples/shell_code.py
+++ examples/shell_code.py
@@ -87,9 +87,9 @@ def spec_conf_nn_this_for_this_prog(nn_in_amount, nn_out_amount):
    nn_params.with_bias = True
    nn_params.with_adap_lr = True
    nn_params.lr = 0.01
-   nn_params.act_fu = RELU
+   nn_params.act_fu = SIGMOID
    nn_params.alpha_sigmoid = 0.056
-   nn_params.mse_treshold=0.01
+   nn_params.mse_treshold=0.14
    # nn_in_amount = 20
    # nn_out_amount = 1
    nn_map = (nn_in_amount, 8, nn_out_amount)
@@ -206,11 +206,11 @@ def vm(buffer:list, level):
             print("in make_train matr X_img",X_img.tolist())
             X_img=np.array(X_img, dtype='float64')
             X_img-=np.mean(X_img, axis=0, dtype='float64')
-            X_img=np.std(X_img, axis=0)
+            # X_img=np.std(X_img, axis=0)
             print("in make train matr",X_img)
-            # Y_img=[[1],[1],[1],[1]]
-            Y_img=[[1]]
-            fit(buffer_ser, nn_params, 10, [X_img.tolist()], Y_img, [X_img.tolist()], Y_img, 100)
+            Y_img=[[1],[1],[1],[1]]
+            # Y_img=[[1]]
+            fit(buffer_ser, nn_params, 10, X_img.tolist(), Y_img, X_img.tolist(), Y_img, 100)
             to_file(nn_params, buffer_ser, nn_params.net, 2, 'img_wei.my')
         elif op == make_img:
             out_nn=answer_nn_direct_on_contrary(nn_params_new, [1], 1)
